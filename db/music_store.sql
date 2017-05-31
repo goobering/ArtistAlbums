4ac768e1e2cb12717ac31a1beed8f10026633eb0
goobering <goobering@gmail.com>
2017-05-31 14:49:19 +0100

    Added sql_runner.rb, music_store.sql

---
+++ db/music_store.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
---
+++ models/artist.rb
@@ -0,0 +1,3 @@
+require 'pry-byebug'
+require_relative '../db/sql_runner'
+
